43bfa4f8c1b89f57f7cfe4a67dc0b0ba7d98e582
ceren.tueker <ceren.tueker@deptagency.com>
2021-02-06 19:20:18 +0100

    function declaration assigned to const for consistency

--- src/components/EventsHeader.js
+++ src/components/EventsHeader.js
@@ -5,7 +5,7 @@ import landingImage from '../images/landingImage.svg';
 /**
  * The page header that displays the berlin logo
  */
-function EventsHeader(){
+const EventsHeader = () => {
   return(
     <header className='app-header fill-window'>
       <img className='app-logo' src={landingImage} alt="Berlin Logo" />
